ad99c4d047e5079e8dbf3fb048bfa23afe442671
eleven-Luk <claired2240@gmail.com>
2026-04-09 13:28:04 +0800

    path

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { body } from 'express-validator';
-import { registerUser, loginUser, resendOTP } from '../../controllers/authController.js';
+import { registerUser, loginUser, resendOTP } from '../controllers/authController.js';
 import { protect } from '../../middleware/auth.js';
 
 const router = express.Router();
